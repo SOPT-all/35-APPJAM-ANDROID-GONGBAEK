6ac4ca4f38a42e18188a56dd33185fb55ab83505
wjdrjs00 <kimjk3409@gmail.com>
2025-01-09 04:59:22 +0900

    [feat] GongBaekTheme 색상 및 타이포그래피 추가

--- app/src/main/java/com/sopt/gongbaek/ui/theme/Theme.kt
+++ app/src/main/java/com/sopt/gongbaek/ui/theme/Theme.kt
@@ -1,57 +1,60 @@
 package com.sopt.gongbaek.ui.theme
 
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
+import android.app.Activity
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+object GongBaekTheme {
+    val colors: GongBaekColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalGongBaekColors.current
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    val typography: GongBaekTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalGongBaekTypography.current
+}
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+@Composable
+fun ProvideGongBaekColorsAndTypography(
+    colors: GongBaekColors,
+    typography: GongBaekTypography,
+    content: @Composable () -> Unit
+) {
+    CompositionLocalProvider(
+        LocalGongBaekColors provides colors,
+        LocalGongBaekTypography provides typography,
+        content = content
+    )
+}
 
 @Composable
 fun GONGBAEKTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    ProvideGongBaekColorsAndTypography(
+        colors = defaultGongBaekColors,
+        typography = defaultGongBaekTypography
+    ) {
+        val view = LocalView.current
+        if (!view.isInEditMode) {
+            SideEffect {
+                (view.context as Activity).window.run {
+                    statusBarColor = white.toArgb()
+                    WindowCompat.getInsetsController(this, view).isAppearanceLightStatusBars = true
+                }
+            }
         }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
         content = content
     )
 }

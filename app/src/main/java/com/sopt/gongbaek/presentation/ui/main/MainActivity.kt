c2f6fbccd4f292c2bcda85eea14d59c3ed0f8619
SYAAINN <alswo08319@gmail.com>
2025-01-10 15:03:27 +0900

    [chore] 화면모드를 세로모드로 제한하는 코드를 개선합니다.

--- app/src/main/java/com/sopt/gongbaek/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/main/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.sopt.gongbaek.presentation.ui.main
 
+import android.annotation.SuppressLint
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,8 +16,10 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @SuppressLint("SourceLockedOrientationActivity")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
         enableEdgeToEdge()
         setContent {
             GONGBAEKTheme {

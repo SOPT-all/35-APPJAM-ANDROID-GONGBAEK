56c7c9ea97f44adafcbde9e5e6c8321390dffcc1
SYAAINN <alswo08319@gmail.com>
2025-01-10 14:35:38 +0900

    [add] 모임등록 스크린 파일들을 생성합니다.

---
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/groupregister/screen/GroupCategoryScreen.kt
@@ -0,0 +1,36 @@
+package com.sopt.gongbaek.presentation.ui.groupregister.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun GroupCategoryRoute() {
+
+}
+
+@Composable
+fun GroupCategoryScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "모임등록 > 카테고리 선택 화면"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShowGroupCategoryScreen() {
+    GroupCategoryScreen()
+}
---
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/groupregister/screen/GroupCycleScreen.kt
@@ -0,0 +1,39 @@
+package com.sopt.gongbaek.presentation.ui.groupregister.screen
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.sopt.gongbaek.ui.theme.GONGBAEKTheme
+
+@Composable
+fun GroupCycleRoute() {
+
+}
+
+@Composable
+fun GroupCycleScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "모임등록 > 활동주기 선택 화면"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShowGroupCycleScreen() {
+    GONGBAEKTheme {
+        GroupCycleScreen()
+    }
+}

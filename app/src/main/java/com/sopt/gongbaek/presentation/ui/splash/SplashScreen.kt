167ffc74a05a0341c966edcd64576a01da5a0bf2
SYAAINN <alswo08319@gmail.com>
2025-01-10 14:31:52 +0900

    [add] 스플래시 스크린 파일을 생성합니다.

---
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/splash/SplashScreen.kt
@@ -0,0 +1,40 @@
+package com.sopt.gongbaek.presentation.ui.splash
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.sopt.gongbaek.ui.theme.GONGBAEKTheme
+
+@Composable
+fun SplashScreen(
+    navController: NavHostController,
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "스플래시 화면"
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShowSplashScreen() {
+    GONGBAEKTheme {
+        SplashScreen(
+            navController = rememberNavController()
+        )
+    }
+}

1703d983e73e87b4f983164c1d146f742ede480a
SYAAINN <alswo08319@gmail.com>
2025-01-11 18:48:57 +0900

    [chore] 프리뷰 코드에 스크린 호출부를 추가합니다.

--- app/src/main/java/com/sopt/gongbaek/presentation/ui/groupregister/screen/GroupRegisterScreen.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/groupregister/screen/GroupRegisterScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.sopt.gongbaek.ui.theme.GONGBAEKTheme
 
 @Composable
 fun GroupRegisterRoute() {
@@ -32,4 +33,7 @@ fun GroupRegisterScreen() {
 @Preview(showBackground = true)
 @Composable
 fun ShowGroupRegisterScreen() {
+    GONGBAEKTheme {
+        GroupRegisterScreen()
+    }
 }

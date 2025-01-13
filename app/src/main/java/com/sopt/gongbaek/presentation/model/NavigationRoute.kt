3848b3d834eeecd5ab604844d1ab0901b1d4b9c3
SYAAINN <alswo08319@gmail.com>
2025-01-14 05:09:21 +0900

    [refactor] Navigation Route를 상수화합니다.

--- app/src/main/java/com/sopt/gongbaek/presentation/model/NavigationRoute.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/model/NavigationRoute.kt
@@ -1,11 +1,58 @@
 package com.sopt.gongbaek.presentation.model
 
-sealed interface Route
+object NavigationRoute {
+    object MainBottomNavBarTabRoute {
+        const val GROUP_LIST_TAB = "group_list_route"
+        const val MY_GROUP_TAB = "my_group_route"
+        const val HOME_TAB = "home_route"
+        const val TIMETABLE_TAB = "timetable_route"
+        const val MY_PAGE_TAB = "my_page_route"
+    }
 
-sealed interface MainBottomNavBarTabRoute : Route {
-    data object GroupList : MainBottomNavBarTabRoute
-    data object MyGroup : MainBottomNavBarTabRoute
-    data object Home : MainBottomNavBarTabRoute
-    data object TimeTable : MainBottomNavBarTabRoute
-    data object MyPage : MainBottomNavBarTabRoute
+    object GroupDetailNavGraphRoute {
+        const val GROUP_DETAIL = "group_detail"
+    }
+
+    object GroupListNavGraphRoute {
+        const val GROUP_LIST = "group_list"
+    }
+
+    object GroupRegisterNavGraphRoute {
+        const val GROUP_CYCLE = "group_cycle"
+        const val SELECT_DAY = "select_day"
+        const val SELECT_DAY_OF_WEEK = "select_day_of_week"
+        const val GROUP_TIME = "group_time"
+        const val GROUP_CATEGORY = "group_category"
+        const val GROUP_COVER = "group_cover"
+        const val GROUP_PLACE_PEOPLE = "group_place_people"
+        const val GROUP_INTRODUCTION = "group_introduction"
+        const val GROUP_REGISTER = "group_register"
+    }
+
+    object HomeNavGraphNavGraphRoute {
+        const val HOME = "home"
+    }
+
+    object MyGroupNavGraphRoute {
+        const val MY_GROUP = "my_group"
+    }
+
+    object OnboardingNavGraphRoute {
+        const val SELECT_PROFILE = "select_profile"
+        const val NICKNAME = "nickname"
+        const val UNIV_MAJOR = "univ_major"
+        const val UNIV_SEARCH = "univ_search"
+        const val MAJOR_SEARCH = "major_search"
+        const val GRADE = "grade"
+        const val MBTI = "mbti"
+        const val GENDER = "gender"
+        const val SELF_INTRODUCTION = "self_introduction"
+        const val ENTER_TIMETABLE = "enter_timetable"
+        const val GAP_TIMETABLE = "gap_timetable"
+        const val COMPLETE_ONBOARDING = "complete_onboarding"
+    }
+
+    object SplashRoute {
+        const val SPLASH = "splash"
+    }
 }

c754e835b7c3f9c507290a4d69f096cc8e1a05ef
MinseoSONG <mins041600@naver.com>
2025-01-15 16:07:21 +0900

    [feat] GroupInfoChipContents를 구현합니다.

---
+++ app/src/main/java/com/sopt/gongbaek/domain/type/GroupInfoChipContents.kt
@@ -0,0 +1,9 @@
+package com.sopt.gongbaek.domain.type
+
+enum class GroupInfoChipContents(
+    val contents: String
+) {
+    STATUS(contents = "status"),
+    CATEGORY(contents = "category"),
+    GROUPTYPE(contents = "groupType")
+}

d910a69e9bc2641c92015d7e940e48a3cce9e324
SYAAINN <alswo08319@gmail.com>
2025-01-16 03:05:33 +0900

    [feat] 완성된 mbti를 만들 util 함수를 구현합니다.

---
+++ app/src/main/java/com/sopt/gongbaek/presentation/util/extension/MbtiCreator.kt
@@ -0,0 +1,8 @@
+package com.sopt.gongbaek.presentation.util.extension
+
+fun createMbti(
+    firstLetter: String,
+    secondLetterType: String,
+    thirdLetterType: String,
+    fourthLetterType: String
+) = firstLetter + secondLetterType + thirdLetterType + fourthLetterType

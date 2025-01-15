6effcb780bc11eb4f6cde98240362d67cffa1ecb
SYAAINN <alswo08319@gmail.com>
2025-01-16 03:05:15 +0900

    [feat] SelectableButtonType을 구현합니다.

---
+++ app/src/main/java/com/sopt/gongbaek/presentation/type/SelectableButtonType.kt
@@ -0,0 +1,80 @@
+package com.sopt.gongbaek.presentation.type
+
+import androidx.annotation.ColorRes
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import com.sopt.gongbaek.domain.type.DayOfWeekType
+import com.sopt.gongbaek.domain.type.GenderType
+import com.sopt.gongbaek.domain.type.GradeType
+import com.sopt.gongbaek.domain.type.GroupCycleType
+import com.sopt.gongbaek.domain.type.MbtiFirstLetterType
+import com.sopt.gongbaek.domain.type.MbtiFourthLetterType
+import com.sopt.gongbaek.domain.type.MbtiSecondLetterType
+import com.sopt.gongbaek.domain.type.MbtiThirdLetterType
+import com.sopt.gongbaek.ui.theme.defaultGongBaekColors
+import com.sopt.gongbaek.ui.theme.defaultGongBaekTypography
+
+enum class SelectableButtonType(
+    val options: List<String>,
+    val chunkedCount: Int = 2,
+    val typoStyle: TextStyle = defaultGongBaekTypography.body1.m16,
+    @ColorRes val selectedButtonColor: Color = defaultGongBaekColors.subOrange,
+    @ColorRes val selectedTextColor: Color = defaultGongBaekColors.mainOrange,
+    @ColorRes val unSelectedButtonColor: Color = defaultGongBaekColors.gray01,
+    @ColorRes val unSelectedTextColor: Color = defaultGongBaekColors.gray07
+) {
+    GRADE(
+        options = listOf(
+            GradeType.FRESHMAN.description,
+            GradeType.SOPHOMORE.description,
+            GradeType.JUNIOR.description,
+            GradeType.SENIOR.description
+        )
+    ),
+    MBTI_FIRST(
+        options = listOf(
+            MbtiFirstLetterType.E.description,
+            MbtiFirstLetterType.I.description
+        )
+    ),
+    MBTI_SECOND(
+        options = listOf(
+            MbtiSecondLetterType.N.description,
+            MbtiSecondLetterType.S.description
+        )
+    ),
+    MBTI_THIRD(
+        options = listOf(
+            MbtiThirdLetterType.F.description,
+            MbtiThirdLetterType.T.description
+        )
+    ),
+    MBTI_FOURTH(
+        options = listOf(
+            MbtiFourthLetterType.P.description,
+            MbtiFourthLetterType.J.description
+        )
+    ),
+    GENDER(
+        options = listOf(
+            GenderType.MAN.description,
+            GenderType.WOMAN.description
+        )
+    ),
+    GROUP_CYCLE(
+        options = listOf(
+            GroupCycleType.ONCE.description,
+            GroupCycleType.WEEKLY.description
+        )
+    ),
+    DAY_OF_WEEK(
+        options = listOf(
+            DayOfWeekType.MON.description,
+            DayOfWeekType.TUE.description,
+            DayOfWeekType.WED.description,
+            DayOfWeekType.THU.description,
+            DayOfWeekType.FRI.description
+        ),
+        chunkedCount = 1
+    )
+}

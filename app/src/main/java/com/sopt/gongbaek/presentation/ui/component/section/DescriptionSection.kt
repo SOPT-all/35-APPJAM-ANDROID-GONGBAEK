7e8ef1498d015a4a20303b2460df09f71e1206b1
MinseoSONG <mins041600@naver.com>
2025-01-16 02:46:29 +0900

    [chore] DescriptionSection을 수정합니다.

--- app/src/main/java/com/sopt/gongbaek/presentation/ui/component/section/DescriptionSection.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/component/section/DescriptionSection.kt
@@ -3,6 +3,7 @@ package com.sopt.gongbaek.presentation.ui.component.section
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,6 +12,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sopt.gongbaek.R
+import com.sopt.gongbaek.presentation.ui.component.button.GongBaekBasicButton
 import com.sopt.gongbaek.ui.theme.GongBaekTheme
 
 @Composable
@@ -19,19 +21,23 @@ fun DescriptionSection(
     modifier: Modifier = Modifier,
     descriptionResId: Int? = null
 ) {
-    Text(
-        text = stringResource(titleResId),
-        color = GongBaekTheme.colors.gray10,
-        style = GongBaekTheme.typography.head2.b24
-    )
-
-    if (descriptionResId != null) {
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ){
         Text(
-            text = stringResource(descriptionResId),
-            color = GongBaekTheme.colors.gray07,
-            style = GongBaekTheme.typography.body1.m16,
-            modifier = Modifier.padding(top = 4.dp)
+            text = stringResource(titleResId),
+            color = GongBaekTheme.colors.gray10,
+            style = GongBaekTheme.typography.head2.b24
         )
+
+        if (descriptionResId != null) {
+            Text(
+                text = stringResource(descriptionResId),
+                color = GongBaekTheme.colors.gray07,
+                style = GongBaekTheme.typography.body1.m16,
+                modifier = Modifier.padding(top = 4.dp)
+            )
+        }
     }
 }
 
@@ -41,6 +47,7 @@ private fun PreviewDescriptionSection1() {
     Column(
         modifier = Modifier
             .background(color = GongBaekTheme.colors.white)
+            .padding(20.dp)
             .fillMaxSize()
     ) {
         DescriptionSection(
@@ -56,6 +63,7 @@ private fun PreviewDescriptionSection2() {
     Column(
         modifier = Modifier
             .background(color = GongBaekTheme.colors.white)
+            .padding(20.dp)
             .fillMaxSize()
     ) {
         DescriptionSection(

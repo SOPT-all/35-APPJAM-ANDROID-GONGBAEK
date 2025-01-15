c4ca58fdb9ab32d0825c2e77e82dce4006bc1ee5
MinseoSONG <mins041600@naver.com>
2025-01-15 18:49:15 +0900

    [chore] color 접근 방식을 수정합니다.

--- app/src/main/java/com/sopt/gongbaek/presentation/type/GroupInfoChipType.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/type/GroupInfoChipType.kt
@@ -3,37 +3,27 @@ package com.sopt.gongbaek.presentation.type
 import androidx.annotation.StringRes
 import androidx.compose.ui.graphics.Color
 import com.sopt.gongbaek.R
-import com.sopt.gongbaek.ui.theme.gray01
-import com.sopt.gongbaek.ui.theme.gray02
-import com.sopt.gongbaek.ui.theme.gray06
-import com.sopt.gongbaek.ui.theme.gray07
-import com.sopt.gongbaek.ui.theme.gray08
-import com.sopt.gongbaek.ui.theme.gray09
-import com.sopt.gongbaek.ui.theme.mainOrange
-import com.sopt.gongbaek.ui.theme.subBlue
-import com.sopt.gongbaek.ui.theme.subGreen
-import com.sopt.gongbaek.ui.theme.subOrange
-import com.sopt.gongbaek.ui.theme.white
+import com.sopt.gongbaek.ui.theme.defaultGongBaekColors
 
 enum class GroupInfoChipType(
     @StringRes val label: Int,
-    val backgroundColor: Color = subOrange,
-    val fontColor: Color = mainOrange
+    val backgroundColor: Color = defaultGongBaekColors.subOrange,
+    val fontColor: Color = defaultGongBaekColors.mainOrange
 ) {
     RECRUITING(
         label = R.string.chip_status_recruiting,
-        backgroundColor = gray08,
-        fontColor = white
+        backgroundColor = defaultGongBaekColors.gray08,
+        fontColor = defaultGongBaekColors.white
     ),
     RECRUITED(
         label = R.string.chip_status_recruited,
-        backgroundColor = gray06,
-        fontColor = white
+        backgroundColor = defaultGongBaekColors.gray06,
+        fontColor = defaultGongBaekColors.white
     ),
     CLOSED(
         label = R.string.chip_status_closed,
-        backgroundColor = gray02,
-        fontColor = gray07
+        backgroundColor = defaultGongBaekColors.gray02,
+        fontColor = defaultGongBaekColors.gray07
     ),
     STUDY(
         label = R.string.chip_category_study
@@ -55,43 +45,43 @@ enum class GroupInfoChipType(
     ),
     STUDY_HOME(
         label = R.string.chip_category_study,
-        backgroundColor = gray09,
-        fontColor = white
+        backgroundColor = defaultGongBaekColors.gray09,
+        fontColor = defaultGongBaekColors.white
     ),
     DINING_HOME(
         label = R.string.chip_category_dining,
-        backgroundColor = gray09,
-        fontColor = white
+        backgroundColor = defaultGongBaekColors.gray09,
+        fontColor = defaultGongBaekColors.white
     ),
     EXERCISE_HOME(
         label = R.string.chip_category_exercise,
-        backgroundColor = gray09,
-        fontColor = white
+        backgroundColor = defaultGongBaekColors.gray09,
+        fontColor = defaultGongBaekColors.white
     ),
     PLAYING_HOME(
         label = R.string.chip_category_playing,
-        backgroundColor = gray09,
-        fontColor = white
+        backgroundColor = defaultGongBaekColors.gray09,
+        fontColor = defaultGongBaekColors.white
     ),
     NETWORKING_HOME(
         label = R.string.chip_category_networking,
-        backgroundColor = gray09,
-        fontColor = white
+        backgroundColor = defaultGongBaekColors.gray09,
+        fontColor = defaultGongBaekColors.white
     ),
     OTHERS_HOME(
         label = R.string.chip_category_others,
-        backgroundColor = gray09,
-        fontColor = white
+        backgroundColor = defaultGongBaekColors.gray09,
+        fontColor = defaultGongBaekColors.white
     ),
     WEEKLY(
         label = R.string.chip_grouptype_weekly,
-        backgroundColor = gray01,
-        fontColor = subGreen
+        backgroundColor = defaultGongBaekColors.gray01,
+        fontColor = defaultGongBaekColors.subGreen
 
     ),
     ONCE(
         label = R.string.chip_grouptype_once,
-        backgroundColor = gray01,
-        fontColor = subBlue
+        backgroundColor = defaultGongBaekColors.gray01,
+        fontColor = defaultGongBaekColors.subBlue
     )
 }

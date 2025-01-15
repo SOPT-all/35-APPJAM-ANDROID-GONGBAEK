0456b8d7b94d962266045da9e98403ea60365d45
MinseoSONG <mins041600@naver.com>
2025-01-15 16:07:39 +0900

    [feat] GroupInfoChipType을 구현합니다.

---
+++ app/src/main/java/com/sopt/gongbaek/presentation/type/GroupInfoChipType.kt
@@ -0,0 +1,79 @@
+package com.sopt.gongbaek.presentation.type
+
+import androidx.annotation.StringRes
+import androidx.compose.ui.graphics.Color
+import com.sopt.gongbaek.R
+import com.sopt.gongbaek.domain.type.GroupInfoChipContents
+import com.sopt.gongbaek.ui.theme.gray01
+import com.sopt.gongbaek.ui.theme.gray02
+import com.sopt.gongbaek.ui.theme.gray06
+import com.sopt.gongbaek.ui.theme.gray07
+import com.sopt.gongbaek.ui.theme.gray08
+import com.sopt.gongbaek.ui.theme.mainOrange
+import com.sopt.gongbaek.ui.theme.subBlue
+import com.sopt.gongbaek.ui.theme.subGreen
+import com.sopt.gongbaek.ui.theme.subOrange
+import com.sopt.gongbaek.ui.theme.white
+
+enum class GroupInfoChipType(
+    val chipContents: GroupInfoChipContents,
+    @StringRes val label: Int,
+    val backgroundColor: Color = subOrange,
+    val fontColor: Color = mainOrange
+) {
+    RECRUITING(
+        chipContents = GroupInfoChipContents.STATUS,
+        label = R.string.chip_status_recruiting,
+        backgroundColor = gray08,
+        fontColor = white
+    ),
+    RECRUITED(
+        chipContents = GroupInfoChipContents.STATUS,
+        label = R.string.chip_status_recruited,
+        backgroundColor = gray06,
+        fontColor = white
+    ),
+    CLOSED(
+        chipContents = GroupInfoChipContents.STATUS,
+        label = R.string.chip_status_closed,
+        backgroundColor = gray02,
+        fontColor = gray07
+    ),
+    STUDY(
+        chipContents = GroupInfoChipContents.CATEGORY,
+        label = R.string.chip_category_study,
+    ),
+    DINING(
+        chipContents = GroupInfoChipContents.CATEGORY,
+        label = R.string.chip_category_dining,
+    ),
+    EXERCISE(
+        chipContents = GroupInfoChipContents.CATEGORY,
+        label = R.string.chip_category_exercise,
+    ),
+    PLAYING(
+        chipContents = GroupInfoChipContents.CATEGORY,
+        label = R.string.chip_category_playing,
+    ),
+    NETWORKING(
+        chipContents = GroupInfoChipContents.CATEGORY,
+        label = R.string.chip_category_networking,
+    ),
+    OTHERS(
+        chipContents = GroupInfoChipContents.CATEGORY,
+        label = R.string.chip_category_others,
+    ),
+    WEEKLY(
+        chipContents = GroupInfoChipContents.GROUPTYPE,
+        label = R.string.chip_grouptype_weekly,
+        backgroundColor = gray01,
+        fontColor = subGreen
+
+    ),
+    ONCE(
+        chipContents = GroupInfoChipContents.GROUPTYPE,
+        label = R.string.chip_grouptype_once,
+        backgroundColor = gray01,
+        fontColor = subBlue
+    ),
+}

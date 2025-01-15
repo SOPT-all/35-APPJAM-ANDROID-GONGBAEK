9eabcf57398ff987be9530ad845376e520ff2906
MinseoSONG <mins041600@naver.com>
2025-01-16 00:20:29 +0900

    [feat] GongBaekDialogType을 구현합니다.

---
+++ app/src/main/java/com/sopt/gongbaek/presentation/type/GongBaekDialogType.kt
@@ -0,0 +1,29 @@
+package com.sopt.gongbaek.presentation.type
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import com.sopt.gongbaek.R
+
+enum class GongBaekDialogType(
+    @StringRes val titleResId: Int,
+    @DrawableRes val imageResId: Int,
+    @StringRes val descriptionResId: Int = R.string.dialog_description_enter_success
+) {
+    REGISTER_SUCCESS(
+        titleResId = R.string.dialog_title_register_success,
+        imageResId = R.drawable.img_image_button_sample,
+    ),
+    REGISTER_FAIL(
+        titleResId = R.string.dialog_title_register_fail,
+        imageResId = R.drawable.img_image_button_sample,
+    ),
+    ENTER_SUCCESS(
+        titleResId = R.string.dialog_title_enter_success,
+        imageResId = R.drawable.img_image_button_sample
+    ),
+    ENTER_FAIL(
+        titleResId = R.string.dialog_title_enter_fail,
+        imageResId = R.drawable.img_image_button_sample,
+        descriptionResId = R.string.dialog_description_enter_fail
+    ),
+}

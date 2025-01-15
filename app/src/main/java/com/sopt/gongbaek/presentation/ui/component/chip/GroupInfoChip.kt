78f08523e2b155774e0b1ff81713562f695395c3
MinseoSONG <mins041600@naver.com>
2025-01-15 18:51:04 +0900

    [chore] shape 방식을 수정합니다.

--- app/src/main/java/com/sopt/gongbaek/presentation/ui/component/chip/GroupInfoChip.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/component/chip/GroupInfoChip.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -25,8 +24,10 @@ fun GroupInfoChip(
         text = stringResource(groupInfoChipType.label),
         color = groupInfoChipType.fontColor,
         modifier = modifier
-            .clip(RoundedCornerShape(2.dp))
-            .background(color = groupInfoChipType.backgroundColor)
+            .background(
+                color = groupInfoChipType.backgroundColor,
+                shape = RoundedCornerShape(2.dp)
+            )
             .padding(horizontal = 4.dp, vertical = 1.dp)
     )
 }

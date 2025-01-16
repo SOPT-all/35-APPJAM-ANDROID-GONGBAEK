ce9c0ff35637b57ff3d5455e0ad2678a3e3e3665
SYAAINN <alswo08319@gmail.com>
2025-01-17 03:29:22 +0900

    [refactor] Domain Layer model을 개선합니다.

--- app/src/main/java/com/sopt/gongbaek/domain/model/GroupComment.kt
+++ app/src/main/java/com/sopt/gongbaek/domain/model/GroupComment.kt
@@ -3,4 +3,23 @@ package com.sopt.gongbaek.domain.model
 data class GroupComment(
     val commentCount: Int,
     val commentList: List<Comment>
-)
+) {
+    data class Comment(
+        val groupId: Int,
+        val groupType: String,
+        val commentId: Int,
+        val commentWriter: String,
+        val commentContent: String,
+        val createdAt: String,
+        val isGroupHost: Boolean,
+        val isWriter: Boolean
+    ) {
+        fun getCreatedMonth(): String = createdAt.split("-")[1]
+
+        fun getCreatedDay(): String = createdAt.split("-")[2]
+
+        fun getCreatedHour(): String = createdAt.split("-")[3]
+
+        fun getCreatedMinute(): String = createdAt.split("-")[4]
+    }
+}

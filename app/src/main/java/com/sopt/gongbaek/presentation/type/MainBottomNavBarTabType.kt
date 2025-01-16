1b8d53bc5f0a02f1bf89f77f955e7033c28198b5
SYAAINN <alswo08319@gmail.com>
2025-01-17 05:37:13 +0900

    [chore] 확정된 바텀 네비게이션 UI를 구현합니다.

--- app/src/main/java/com/sopt/gongbaek/presentation/type/MainBottomNavBarTabType.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/type/MainBottomNavBarTabType.kt
@@ -6,32 +6,38 @@ import com.sopt.gongbaek.R
 import com.sopt.gongbaek.presentation.model.NavigationRoute
 
 enum class MainBottomNavBarTabType(
-    @DrawableRes val iconRes: Int,
+    @DrawableRes val selectedIconRes: Int,
+    @DrawableRes val unselectedIconRes: Int,
     @StringRes val label: Int,
     val route: String
 ) {
     GROUP_LIST(
-        iconRes = R.drawable.ic_android_black_24dp,
+        selectedIconRes = R.drawable.ic_navi_fill_black_26,
+        unselectedIconRes = R.drawable.ic_navi_fill_gray_26,
         label = R.string.main_bottom_navbar_group_list,
         route = NavigationRoute.MainBottomNavBarTabRoute.GROUP_LIST_TAB
     ),
     MY_GROUP(
-        iconRes = R.drawable.ic_android_black_24dp,
+        selectedIconRes = R.drawable.ic_navi_my_fill_black_26,
+        unselectedIconRes = R.drawable.ic_navi_my_fill_gray_26,
         label = R.string.main_bottom_navbar_my_group,
         route = NavigationRoute.MainBottomNavBarTabRoute.MY_GROUP_TAB
     ),
     HOME(
-        iconRes = R.drawable.ic_android_black_24dp,
+        selectedIconRes = R.drawable.ic_navi_home_black_26,
+        unselectedIconRes = R.drawable.ic_navi_home_gray_26,
         label = R.string.main_bottom_navbar_home,
         route = NavigationRoute.MainBottomNavBarTabRoute.HOME_TAB
     ),
     TIMETABLE(
-        iconRes = R.drawable.ic_android_black_24dp,
+        selectedIconRes = R.drawable.ic_navi_timetable_black_26,
+        unselectedIconRes = R.drawable.ic_navi_timetable_gray_26,
         label = R.string.main_bottom_navbar_timetable,
         route = NavigationRoute.MainBottomNavBarTabRoute.TIMETABLE_TAB
     ),
     MY_PAGE(
-        iconRes = R.drawable.ic_android_black_24dp,
+        selectedIconRes = R.drawable.ic_navi_my_black_26,
+        unselectedIconRes = R.drawable.ic_navi_my_gray_26,
         label = R.string.main_bottom_navbar_my_page,
         route = NavigationRoute.MainBottomNavBarTabRoute.MY_PAGE_TAB
     );

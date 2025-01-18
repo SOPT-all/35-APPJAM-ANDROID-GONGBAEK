220668dcccd2cec7da528335b881ea89ac76741e
SYAAINN <alswo08319@gmail.com>
2025-01-18 16:28:30 +0900

    [feat] "나의채움"에서 사용될 TabPager의 타입을 생성합니다.

---
+++ app/src/main/java/com/sopt/gongbaek/presentation/type/MyGroupPagerType.kt
@@ -0,0 +1,12 @@
+package com.sopt.gongbaek.presentation.type
+
+enum class MyGroupPagerType(
+    val description: String
+) {
+    MY_REGISTER(
+        description = "내가 모집한"
+    ),
+    MY_APPLY(
+        description = "내가 신청한"
+    )
+}

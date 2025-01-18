47ccacb5284647abe21c0bf02714cd15bc54bf49
wjdrjs00 <kimjk3409@gmail.com>
2025-01-19 06:04:12 +0900

    [chore] ktlint 수정

--- app/src/main/java/com/sopt/gongbaek/presentation/ui/auth/navigation/AuthNavGraph.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/auth/navigation/AuthNavGraph.kt
@@ -1,10 +1,10 @@
 package com.sopt.gongbaek.presentation.ui.auth.navigation
 
-import androidx.compose.runtime.remember
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.navigation
 import com.sopt.gongbaek.presentation.model.NavigationRoute
 import com.sopt.gongbaek.presentation.ui.auth.screen.AuthViewModel
@@ -21,28 +21,34 @@ fun NavGraphBuilder.authNavGraph(
         composable(
             route = NavigationRoute.AuthNavGraphRoute.SELECT_PROFILE
         ) {
-            val backStackEntry = remember(navController) {
-                navController.getBackStackEntry(NavigationRoute.AuthNavGraphRoute.AUTH_NAV_GRAPH)
+            val viewModel: AuthViewModel? = navController.currentBackStackEntryAsState().value?.let { it1 ->
+                hiltViewModel(
+                    it1
+                )
             }
-            val viewModel: AuthViewModel = hiltViewModel(backStackEntry)
-            SelectProfileRoute(
-                viewModel = viewModel,
-                navigateNickname = navController::navigateNickname
-            )
+            if (viewModel != null) {
+                SelectProfileRoute(
+                    viewModel = viewModel,
+                    navigateNickname = navController::navigateNickname
+                )
+            }
         }
 
         composable(
             route = NavigationRoute.AuthNavGraphRoute.NICKNAME
         ) {
-            val backStackEntry = remember(navController) {
-                navController.getBackStackEntry(NavigationRoute.AuthNavGraphRoute.AUTH_NAV_GRAPH)
+            val viewModel: AuthViewModel? = navController.currentBackStackEntryAsState().value?.let { it1 ->
+                hiltViewModel(
+                    it1
+                )
             }
-            val viewModel: AuthViewModel = hiltViewModel(backStackEntry)
-            NicknameRoute(
-                viewModel = viewModel,
-                navigateUnivMajor = navController::navigateUnivMajor,
-                navigateBack = navController::popBackStack
-            )
+            if (viewModel != null) {
+                NicknameRoute(
+                    viewModel = viewModel,
+                    navigateUnivMajor = navController::navigateUnivMajor,
+                    navigateBack = navController::popBackStack
+                )
+            }
         }
     }
 }

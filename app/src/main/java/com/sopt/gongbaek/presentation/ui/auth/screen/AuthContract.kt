d4d8529e73119bc35cdaec9b53a3718452b5ee4d
SYAAINN <alswo08319@gmail.com>
2025-01-20 03:55:55 +0900

    [chore] 변경된 Domain Layer Entity를 반영합니다.

--- app/src/main/java/com/sopt/gongbaek/presentation/ui/auth/screen/AuthContract.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/auth/screen/AuthContract.kt
@@ -7,28 +7,12 @@ import com.sopt.gongbaek.presentation.util.base.UiSideEffect
 import com.sopt.gongbaek.presentation.util.base.UiState
 
 class AuthContract {
-
     data class State(
-        val userInfo: UserInfo = UserInfo(
-            profileImage = null,
-            nickname = "",
-            mbti = "",
-            school = "",
-            major = "",
-            grade = null,
-            enterYear = null,
-            introduction = "",
-            gender = "",
-            timeTable = TimeTable(
-                weekDay = "",
-                startTime = 0f,
-                endTime = 0f
-            )
-        )
+        val userInfo: UserInfo = UserInfo()
     ) : UiState
 
     sealed class Event : UiEvent {
-        data class OnProfileImageSelected(val profileImage: Int?) : Event()
+        data class OnProfileImageSelected(val profileImage: Int) : Event()
         data class OnNicknameChanged(val nickname: String) : Event()
     }
 

c743fd6a529975be6ae242921efd42daebc3860b
SYAAINN <alswo08319@gmail.com>
2025-01-21 05:18:32 +0900

    [refactor] Domain Entity 들의 초기값을 세팅합니다.

--- app/src/main/java/com/sopt/gongbaek/domain/model/UserInfo.kt
+++ app/src/main/java/com/sopt/gongbaek/domain/model/UserInfo.kt
@@ -1,14 +1,14 @@
 package com.sopt.gongbaek.domain.model
 
 data class UserInfo(
-    val profileImage: Int?,
-    val nickname: String,
-    val mbti: String,
-    val school: String,
-    val major: String,
-    val grade: Int?,
-    val enterYear: Int?,
-    val introduction: String,
-    val gender: String,
-    val timeTable: TimeTable
+    val profileImage: Int = 0,
+    val nickname: String = "",
+    val mbti: String = "",
+    val school: String = "",
+    val major: String = "",
+    val grade: Int = 0,
+    val enterYear: Int = 0,
+    val introduction: String = "",
+    val gender: String = "",
+    val timeTable: TimeTable = TimeTable()
 )

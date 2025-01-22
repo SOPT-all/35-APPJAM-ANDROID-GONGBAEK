c3fe3906bb0f6aab1d592da772ae0801988f2edc
SYAAINN <alswo08319@gmail.com>
2025-01-23 05:51:50 +0900

    [feat] nullable한 baseApiResponse를 정의합니다.

--- app/src/main/java/com/sopt/gongbaek/data/remote/util/ApiResponseHandler.kt
+++ app/src/main/java/com/sopt/gongbaek/data/remote/util/ApiResponseHandler.kt
@@ -1,8 +1,16 @@
 package com.sopt.gongbaek.data.remote.util
 
 import com.sopt.gongbaek.data.remote.dto.base.ApiResponse
+import com.sopt.gongbaek.data.remote.dto.base.NullableApiResponse
 
 fun <T> ApiResponse<T>.handleApiResponse(): Result<T> {
+    return when (this.success) {
+        true -> Result.success(this.data)
+        false -> Result.failure(Exception("Error code : ${this.code}, Error message : ${this.message}"))
+    }
+}
+
+fun <T> NullableApiResponse<T>.handleNullableApiResponse(): Result<T?> {
     return when (this.success) {
         true -> Result.success(this.data)
         false -> Result.failure(Exception("Error code : ${this.code}, Error message : ${this.message}"))

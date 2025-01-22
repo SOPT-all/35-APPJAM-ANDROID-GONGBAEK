6d5e21ea5bc9856609a91bbdbf5d839264fedaa6
SYAAINN <alswo08319@gmail.com>
2025-01-23 07:09:52 +0900

    [feat] 댓글 작성 API를 위한 선언들을 추가합니다.

--- app/src/main/java/com/sopt/gongbaek/data/remote/service/CommentService.kt
+++ app/src/main/java/com/sopt/gongbaek/data/remote/service/CommentService.kt
@@ -1,8 +1,13 @@
 package com.sopt.gongbaek.data.remote.service
 
 import com.sopt.gongbaek.data.remote.dto.base.ApiResponse
+import com.sopt.gongbaek.data.remote.dto.base.NullableApiResponse
+import com.sopt.gongbaek.data.remote.dto.request.PostCommentRequestDto
 import com.sopt.gongbaek.data.remote.dto.response.GroupCommentsResponseDto
+import com.sopt.gongbaek.data.remote.dto.response.PostCommentResponseDto
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface CommentService {
@@ -11,4 +16,9 @@ interface CommentService {
         @Query("groupId") groupId: Int,
         @Query("groupType") groupType: String
     ): ApiResponse<GroupCommentsResponseDto>
+
+    @POST("/api/v1/comment")
+    suspend fun postComment(
+        @Body postCommentRequestDto: PostCommentRequestDto
+    ): NullableApiResponse<PostCommentResponseDto>
 }

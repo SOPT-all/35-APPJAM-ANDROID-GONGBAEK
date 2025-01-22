b1b2e37aeef4e8782fe84085270cd89378296f0e
SYAAINN <alswo08319@gmail.com>
2025-01-23 01:58:46 +0900

    [feat] GroupInfo를 받아오는 BaseResponseDto를 정의합니다.

--- app/src/main/java/com/sopt/gongbaek/data/remote/dto/response/MyGroupsResponseDto.kt
+++ app/src/main/java/com/sopt/gongbaek/data/remote/dto/response/MyGroupsResponseDto.kt
@@ -5,20 +5,5 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class MyGroupsResponseDto(
-    @SerialName("groups") val groups: List<Group>
-) {
-    @Serializable
-    data class Group(
-        @SerialName("groupId") val groupId: Int,
-        @SerialName("coverImg") val coverImg: Int,
-        @SerialName("status") val status: String,
-        @SerialName("category") val category: String,
-        @SerialName("groupType") val groupType: String,
-        @SerialName("groupTitle") val groupTitle: String,
-        @SerialName("weekDate") val weekDate: String?,
-        @SerialName("weekDay") val weekDay: String,
-        @SerialName("startTime") val startTime: Double,
-        @SerialName("endTime") val endTime: Double,
-        @SerialName("location") val location: String
-    )
-}
+    @SerialName("groups") val groups: List<BaseGroupInfoResponseDto>
+)

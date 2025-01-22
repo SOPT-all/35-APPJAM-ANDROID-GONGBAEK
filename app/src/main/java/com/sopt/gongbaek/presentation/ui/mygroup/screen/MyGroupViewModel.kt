cddee8c4b657c8fc5399d95657f2c5e45c756d05
SYAAINN <alswo08319@gmail.com>
2025-01-22 10:16:03 +0900

    [feat] ViewModel에 서버통신 로직을 구현합니다.

--- app/src/main/java/com/sopt/gongbaek/presentation/ui/mygroup/screen/MyGroupViewModel.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/mygroup/screen/MyGroupViewModel.kt
@@ -1,15 +1,25 @@
 package com.sopt.gongbaek.presentation.ui.mygroup.screen
 
+import android.util.Log
 import androidx.lifecycle.viewModelScope
+import com.sopt.gongbaek.domain.usecase.GetMyGroupsUseCase
 import com.sopt.gongbaek.presentation.util.base.BaseViewModel
 import com.sopt.gongbaek.presentation.util.base.UiLoadState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class MyGroupViewModel @Inject constructor() : BaseViewModel<MyGroupContract.State, MyGroupContract.Event, MyGroupContract.SideEffect>() {
+class MyGroupViewModel @Inject constructor(
+    private val getMyGroupsUseCase: GetMyGroupsUseCase
+) : BaseViewModel<MyGroupContract.State, MyGroupContract.Event, MyGroupContract.SideEffect>() {
     override fun createInitialState(): MyGroupContract.State = MyGroupContract.State()
+
+    init {
+        getRegisterGroups()
+        Timber.tag("MyGroupViewModel Init").d("실행됐지롱 ㅋㅋ")
+    }
 
     override suspend fun handleEvent(event: MyGroupContract.Event) {
         when (event) {
@@ -27,14 +37,65 @@ class MyGroupViewModel @Inject constructor() : BaseViewModel<MyGroupContract.Sta
     private fun getRegisterGroups() {
         viewModelScope.launch {
             setState { copy(loadState = UiLoadState.Loading) }
-            // 내가 모집한 모임들을 불러오는 로직
+            getMyGroupsUseCase(category = "REGISTER", status = true).fold(
+                onSuccess = { activeGroups ->
+                    setState {
+                        copy(
+                            loadState = UiLoadState.Success,
+                            activeGroups = activeGroups
+                        )
+                    }
+                    Timber.tag("activeGroups").d("${currentState.activeGroups}")
+                },
+                onFailure = {
+                    setState { copy(loadState = UiLoadState.Error) }
+                }
+            )
+            getMyGroupsUseCase(category = "REGISTER", status = false).fold(
+                onSuccess = { closedGroups ->
+                    setState {
+                        copy(
+                            loadState = UiLoadState.Success,
+                            closedGroups = closedGroups
+                        )
+                    }
+                },
+                onFailure = {
+                    setState { copy(loadState = UiLoadState.Error) }
+                }
+            )
         }
     }
 
     private fun getApplyGroups() {
         viewModelScope.launch {
             setState { copy(loadState = UiLoadState.Loading) }
-            // 내가 신청한 모임들을 불러오는 로직
+            getMyGroupsUseCase(category = "APPLY", status = true).fold(
+                onSuccess = { activeGroups ->
+                    setState {
+                        copy(
+                            loadState = UiLoadState.Success,
+                            activeGroups = activeGroups
+                        )
+                    }
+                },
+                onFailure = {
+                    setState { copy(loadState = UiLoadState.Error) }
+                }
+            )
+            getMyGroupsUseCase(category = "APPLY", status = false).fold(
+                onSuccess = { closedGroups ->
+                    setState {
+                        copy(
+                            loadState = UiLoadState.Success,
+                            closedGroups = closedGroups
+                        )
+                    }
+                },
+                onFailure = {
+                    setState { copy(loadState = UiLoadState.Error) }
+                }
+            )
         }
     }
 }

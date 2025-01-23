072e56d28b123b90000d080f118469da20efec1e
SYAAINN <alswo08319@gmail.com>
2025-01-23 12:05:39 +0900

    [feat] useCase에 대한 의존성을 추가합니다.

--- app/src/main/java/com/sopt/gongbaek/di/UseCaseModule.kt
+++ app/src/main/java/com/sopt/gongbaek/di/UseCaseModule.kt
@@ -1,9 +1,14 @@
 package com.sopt.gongbaek.di
 
+import com.sopt.gongbaek.domain.repository.CommentRepository
 import com.sopt.gongbaek.domain.repository.ExampleRepository
 import com.sopt.gongbaek.domain.repository.GroupRepository
+import com.sopt.gongbaek.domain.usecase.ApplyGroupUseCase
 import com.sopt.gongbaek.domain.usecase.ExamplePostUseCase
+import com.sopt.gongbaek.domain.usecase.GetGroupCommentsUseCase
 import com.sopt.gongbaek.domain.usecase.GetMyGroupsUseCase
+import com.sopt.gongbaek.domain.usecase.LoadGroupDetailScreenUseCase
+import com.sopt.gongbaek.domain.usecase.PostCommentUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -24,4 +29,29 @@ object UseCaseModule {
     fun provideGetMyGroupsUseCase(
         groupRepository: GroupRepository
     ): GetMyGroupsUseCase = GetMyGroupsUseCase(groupRepository)
+
+    @Provides
+    @Singleton
+    fun provideLoadGroupDetailScreenUseCase(
+        groupRepository: GroupRepository,
+        commentRepository: CommentRepository
+    ): LoadGroupDetailScreenUseCase = LoadGroupDetailScreenUseCase(groupRepository, commentRepository)
+
+    @Provides
+    @Singleton
+    fun provideApplyGroupUseCase(
+        groupRepository: GroupRepository
+    ): ApplyGroupUseCase = ApplyGroupUseCase(groupRepository)
+
+    @Provides
+    @Singleton
+    fun provideGetGroupCommentsUseCase(
+        commentRepository: CommentRepository
+    ): GetGroupCommentsUseCase = GetGroupCommentsUseCase(commentRepository)
+
+    @Provides
+    @Singleton
+    fun providesPostCommentUseCase(
+        commentRepository: CommentRepository
+    ): PostCommentUseCase = PostCommentUseCase(commentRepository)
 }

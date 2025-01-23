49b3a198f213b9d95a0e48d9ddce6728714adb75
MinseoSONG <mins041600@naver.com>
2025-01-24 02:56:24 +0900

    [fix] 채우기뷰 요일바 오류를 수정합니다.

--- app/src/main/java/com/sopt/gongbaek/presentation/ui/grouplist/component/DayOfWeekBar.kt
+++ app/src/main/java/com/sopt/gongbaek/presentation/ui/grouplist/component/DayOfWeekBar.kt
@@ -5,14 +5,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sopt.gongbaek.domain.type.DayOfWeekType
@@ -50,7 +48,6 @@ fun DayOfWeekBar(
         contentLists.forEachIndexed { index, content ->
             Box(
                 modifier = modifier
-                    .height((LocalConfiguration.current.screenHeightDp * 0.04f).dp)
                     .background(
                         color = if (selectedIndex == index) GongBaekTheme.colors.white else GongBaekTheme.colors.gray01,
                         shape = RoundedCornerShape(2.dp)
